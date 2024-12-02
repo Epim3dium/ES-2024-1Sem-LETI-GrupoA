d1a60872c061f181c06c34e709af998ec28c49d2
Epim3dium <Epim3dium@protonmail.com>
2024-12-02 21:27:55 +0000

    initialized graph structure for owners

--- src/main/java/iscte/se/landmanagement/OwnerGraphStructure.java
+++ src/main/java/iscte/se/landmanagement/OwnerGraphStructure.java
@@ -13,12 +13,37 @@ import java.util.List;
 public class OwnerGraphStructure {
     private Graph<Integer, DefaultEdge> graph;
     private ArrayList<Integer> owners;
+
+    public void visualizeGraph() {
+        ;
+    }
+
+
     public OwnerGraphStructure(Graph<Property, DefaultEdge> property_neighbours) {
         this.graph = formGraph(property_neighbours);
     }
 
     private Graph<Integer, DefaultEdge> formGraph(Graph<Property, DefaultEdge> property_neighbours) {
         SimpleGraph<Integer, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+
+        for(Property p : property_neighbours.vertexSet()) {
+            if(!g.containsVertex(p.getOwnerID())) {
+                g.addVertex(p.getOwnerID());
+            }
+            for(DefaultEdge e : property_neighbours.edgesOf(p)) {
+                Property neighbour = property_neighbours.getEdgeTarget(e);
+                if(!g.containsVertex(neighbour.getOwnerID())) {
+                    g.addVertex(neighbour.getOwnerID());
+                }
+
+                if(!g.containsEdge(p.getOwnerID(), neighbour.getOwnerID())
+                        && !g.containsEdge(neighbour.getOwnerID(), p.getOwnerID())
+                        && neighbour.getOwnerID() != p.getOwnerID())
+                {
+                    g.addEdge(p.getOwnerID(), neighbour.getOwnerID());
+                }
+            }
+        }
         return g;
     }
 }
