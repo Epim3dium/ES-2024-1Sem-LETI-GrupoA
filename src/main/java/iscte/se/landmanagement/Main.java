1a4e23865b880af4fe472fdbb324c3ce60408128
Joel Matos <jmmso4@iscte-iul.pt>
2024-12-06 18:50:37 +0000

    Update Main to just run the app

--- src/main/java/iscte/se/landmanagement/Main.java
+++ src/main/java/iscte/se/landmanagement/Main.java
@@ -1,5 +1,7 @@
 package iscte.se.landmanagement;
 
+import org.openjfx.es20241semletigrupoa.HelloApplication;
+
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,25 +10,6 @@ import java.nio.file.Paths;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        URL url = Thread.currentThread().getContextClassLoader().getResource("test.csv");
-        if (url == null) {
-            System.out.println("Arquivo CSV não encontrado!");
-            return;
-        }
-
-        Path path = Paths.get(url.toURI());
-        PropFileReader propFileReader = new PropFileReader(path);
-
-        System.out.println("Hello and welcome!");
-
-
-       propFileReader.readFile();
-       propFileReader.convertToPropertiy();
-        System.out.printf("PropertyID: " + Integer.toString(propFileReader.getProperties().get(1).getPropertyID()) + "\t");
-        System.out.printf("OwnerID: "+  Integer.toString(propFileReader.getProperties().get(1).getOwnerID())+"\n");
-        System.out.printf("Island: " + propFileReader.getProperties().get(1).getIsland() + "\t");
-        System.out.print("Parish: " + propFileReader.getProperties().get(1).getParish());
-
-        GraphStructure g = new GraphStructure(propFileReader.getProperties(), 1);
+        HelloApplication.main();
     }
 }
--- src/main/java/org/openjfx/es20241semletigrupoa/HelloApplication.java
+++ src/main/java/org/openjfx/es20241semletigrupoa/HelloApplication.java
@@ -31,7 +31,7 @@ public class HelloApplication extends Application {
 
     }
 
-    public static void main(String[] args) {
+    public static void main() {
         launch();
     }
 }
